587c74600d5c21603bbd045d5344cc5daf7846c7
RinoNaito <rinodelevingne@gmail.com>
2025-04-06 17:13:55 +0900

    MOD: ex00

--- ex00/DateUtils.cpp
+++ ex00/DateUtils.cpp
@@ -5,16 +5,17 @@ bool isLeapYear(int year) {
 }
 
 bool parseDate(const std::string& dateStr, tm &tm_original) {
+	const std::string ERROR_DATE_NOT_EXIST = "Error: date does not exist.";
 	int year, month, day;
 	if (sscanf(dateStr.c_str(), "%d-%d-%d", &year, &month, &day) != 3)
 	  return false;
 	if (month < 1 || month > 12)
-		throw std::runtime_error("Error: date does not exist.");
+		throw std::runtime_error(ERROR_DATE_NOT_EXIST);
 	int last_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31};
 	if (month == 2 && isLeapYear(year))
 		last_day[month - 1] = 29;
 	if (day < 0 || day > last_day[month - 1])
-		throw std::runtime_error("Error: date does not exist.");
+		throw std::runtime_error(ERROR_DATE_NOT_EXIST);
 	tm_original = tm();
 	tm_original.tm_year = year - 1900;
 	tm_original.tm_mon = month - 1;
