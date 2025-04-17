c080f61a73b44bbdaf42c07d9b7cb93147fdca1f
Rino Naito <rnaito@c4r3s13.42tokyo.jp>
2025-04-17 22:43:49 +0900

    save

--- ex00/DateUtils.cpp
+++ ex00/DateUtils.cpp
@@ -4,11 +4,16 @@ bool isLeapYear(int year) {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
+#include <iostream>
 bool parseDate(const std::string& dateStr, tm &tm_original) {
 	const std::string ERROR_DATE_NOT_EXIST = "Error: date does not exist.";
-	int year, month, day;
-	if (sscanf(dateStr.c_str(), "%d-%d-%d", &year, &month, &day) != 3)
-	  return false;
+	if (dateStr.size() != 10 || dateStr[4] != '-'|| dateStr[7] != '-'){
+		std::cout <<" errorDateStr :["<< dateStr << "]"<< std::endl;
+		return false;
+	}
+	int year = atoi(dateStr.substr(0, 4).c_str());
+	int month = atoi(dateStr.substr(5, 2).c_str());
+	int day = atoi(dateStr.substr(8, 2).c_str());
 	if (month < 1 || month > 12)
 		throw std::runtime_error(ERROR_DATE_NOT_EXIST);
 	int last_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31};
